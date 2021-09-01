a22bc8b58aaedd221fdad04b0f17a4fa721f2ca7
nilsnolde <nils@gis-ops.com>
2021-09-01 14:07:14 +0200

    finalize extract script

--- scripts/extras/create_extracts.sh
+++ scripts/extras/create_extracts.sh
@@ -2,30 +2,32 @@
 
 # Create different extracts with osmium based on an osmium config file
 
-. /valhalla/scripts/runtime/env.sh
+. /valhalla/scripts/env.sh
 
 CITY_REPO="canada_cities_osmium_extract"
 
 printf "### Installing Osmium & Git ###\n"
 
 # install or update osmium & git if not installed already
-apt-get update -y > /dev/null && apt-get install -y osmium-tool git > /dev/null
+apt-get update -y  && apt-get install -y osmium-tool git wget
 
-git clone https://github.com/gis-ops/canada_cities_osmium_extract.git ${SCRIPTS_PATH}/${CITY_REPO}
+git clone https://github.com/gis-ops/canada_cities_osmium_extract.git ${SCRIPTS_PATH}/extras/${CITY_REPO}
 
 printf "\n### Downloading OSM extracts ###\n"
 
 for extract in us-midwest us-northeast us-south canada
 do
   fp=${CUSTOM_FILES}/${extract}-latest.osm.pbf
-  if [[ ! -f "$fp" ]]; then
+  if [[ ! -f "${fp}" ]]; then
     echo "Downloading to ${fp}"
-    wget http://download.geofabrik.de/north-america/${extract}-latest.osm.pbf -o ${fp} > /dev/null
+    wget http://download.geofabrik.de/north-america/${extract}-latest.osm.pbf -P ${CUSTOM_FILES} --quiet
+  else
+    echo "${fp} already exists."
   fi
 done
 
 printf "\n### Cutting regions ###\n"
 
-osmmium extract --set-bounds --config ${SCRIPTS_PATH}/${CITY_REPO}/osmium_extract_config.json
+osmium extract --config ${SCRIPTS_PATH}/extras/${CITY_REPO}/osmium_extract_config.json --set-bounds ${CUSTOM_FILES}/canada-latest.osm.pbf
 
 printf "\n### Downloading elevation ###\n"
