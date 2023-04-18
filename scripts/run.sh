57eba559c6743a8984f6e974228219789dbcd507
Christian Beiwinkel <chrstn.bwnkl@gmail.com>
2023-04-18 22:08:09 +0200

    export at assignment

--- scripts/run.sh
+++ scripts/run.sh
@@ -40,9 +40,8 @@ echo ""
 
 # the env vars with True default are set in the dockerfile, others are evaluated in configure_valhalla.sh
 if [[ -z $server_threads ]]; then
-  server_threads=$(nproc)
+  export server_threads=$(nproc)
 fi
-export server_threads
 
 if [[ -z $build_tar ]]; then
   build_tar="True"
